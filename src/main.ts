ea922861b5f6e7dd756f4278049c89fa9de57a36
iSpace <steamdavid2007@gmail.com>
2024-02-27 19:54:16 +0300

    4134134

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,34 @@
 import './style.css'
+                                                                      // пример использования switch
+// let day: number = 6
+// let dayName: string
 
+// switch (day) {
+//     case 1:
+//         dayName = "Понедельник"
+//         break
+//     case 2:
+//         dayName = "Вторник"
+//         break
+//     case 3:
+//         dayName = "Среда"
+//         break
+//     case 4:
+//         dayName = "Четверг"
+//         break
+//     case 5:
+//         dayName = "Пятница"
+//         break
+//     case 6:
+//         dayName = "Суббота"
+//         break
+//     case 7:
+//         dayName = "Воскресенье"
+//         break
+//     default:
+//         dayName = "Некорректный день"
+// }
+// console.log(`Сегодня ${dayName}`);
 
 
 
@@ -8,7 +37,19 @@ import './style.css'
 
 
 
+                                          // пример использования логических операторов
+// let isSunny: boolean = true
+// let temperature: number = 55
 
+// if (isSunny && temperature > 20) {
+//     console.log("Погода отличная для прогулки!")
+// } else if (isSunny && temperature <= 20) {
+//     console.log("Можно пойти на прогулку, но возьми кофту!")
+// } else if (!isSunny) {
+//     console.log("Сегодня пасмурно, лучше остаться дома.")
+// } else {
+//     console.log("Погода неопределенная, решайте сами.")
+// }
 
 
 
@@ -35,6 +76,7 @@ import './style.css'
 
 
 
+ 
 
 
 
@@ -110,6 +152,7 @@ import './style.css'
 
 
 
+                                                               //вводный
 // let nemo = ('makaka')
 // let p = ('Obezyana')
 // console.log('Hello World!!!')
@@ -207,6 +250,7 @@ import './style.css'
 
 
 
+                                                               //скопированный код для калькулятора
 // function getHistory(){
 //   return document.getElementById("history-value").innerText;
 // }
@@ -319,6 +363,7 @@ import './style.css'
 
 
 
+                                                               //оператор if
 // var number = document.getElementsByClassName("number");
 // for(var i =0;i<number.length;i++){
 //   number[i].addEventListener('click',function(){
@@ -349,29 +394,29 @@ import './style.css'
 
 
 
-// // // можно переназначить var
-// // var a = 10
-// // console.log(a)
-// // var a = 20
-// // console.log(a)
-// // // не меняется
-// // let newVar = 5 
-// // console.log(newVar)
-// // // не заданное значение noVal
-// // let noVal
-// // console.log(noVal)
-// // // НЕЛЬЗЯ переназначать константу
-// // const DAY_IN_WEEK = 7
-// // // DAY_IN_WEEK = 8
-// // // нельзя использовать цифры в название переменной
-// // // let 23 
-// // console.log('1+1=',1+1)
-// // console.log('10+1=',10+1)
-// // console.log('5-1=',5-1)
-// // console.log('134*4=',134*4)
-// // console.log('3 в 3 степени=',3**3)
-// // let number1 = prompt("Введите первое число:")
-// // let number2 = prompt("Введите второе число:")
+                                                             // можно переназначить var
+// var a = 10
+// console.log(a)
+// var a = 20
+// console.log(a)
+// // не меняется
+// let newVar = 5 
+// console.log(newVar)
+// // не заданное значение noVal
+// let noVal
+// console.log(noVal)
+// // НЕЛЬЗЯ переназначать константу
+// const DAY_IN_WEEK = 7
+// // DAY_IN_WEEK = 8
+// // нельзя использовать цифры в название переменной
+// // let 23 
+// console.log('1+1=',1+1)
+// console.log('10+1=',10+1)
+// console.log('5-1=',5-1)
+// console.log('134*4=',134*4)
+// console.log('3 в 3 степени=',3**3)
+// let number1 = prompt("Введите первое число:")
+// let number2 = prompt("Введите второе число:")
 
 
 
@@ -379,27 +424,12 @@ import './style.css'
 
 
 
-
+                                                                                    //калькулятор 
 // // let sum = parseFloat(number1) + parseFloat(number2)
 // // let raznost = parseFloat(number1) - parseFloat(number2)
 // // let del = parseFloat(number1) / parseFloat(number2)
 // // let ymnoj = parseFloat(number1) * parseFloat(number2)
 // // let stepen = parseFloat(number1) ** parseFloat(number2)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // // alert('Сумма чисел равна: ' + sum +'\nРазность чисел равна:' +raznost +'\nПри деление:'+ del +'\nПри умножении:'+ymnoj+'\nПервое число в степени второго числа: '+stepen)
 // alert("Задание 1 нажмите ок чтобы продолжить")
 // let name = prompt('Введите ваше имя: ')
@@ -419,7 +449,7 @@ import './style.css'
 
 
 
-// alert("Задание 2 нажмите ок чтобы продолжить")
+                // alert("Задание 2 нажмите ок чтобы продолжить")
 // const DATE = 2024
 // let date_of_born = prompt('Введите свою дату рождения') as string
 // alert('Сейчас вам: '+ (DATE - +date_of_born))
@@ -433,7 +463,7 @@ import './style.css'
 
 
 
-// alert("Задание 3 нажмите ок чтобы продолжить")
+               // alert("Задание 3 нажмите ок чтобы продолжить")
 // let perimetr = prompt('Введите длину стороны квадрата') as string
 // alert('Периметр квадрата = '+ Number(perimetr)*4)
 
@@ -456,7 +486,7 @@ import './style.css'
 
 
 
-// alert("Задание 4 нажмите ок чтобы продолжить")
+                // alert("Задание 4 нажмите ок чтобы продолжить")
 // let radius = prompt('Введите радиус окружности') as string
 // alert(Math.PI * +radius * +radius )
 
@@ -475,7 +505,7 @@ import './style.css'
 
 
 
-// alert("Задание 5 нажмите ок чтобы продолжить")
+                           // alert("Задание 5 нажмите ок чтобы продолжить")
 // let distance = prompt("Введите расстояние между городами в километрах:")
 // let time = prompt("Введите время, за которое вы хотите добраться (в часах):") as string
 // let speed = (+distance / +time)
@@ -492,7 +522,7 @@ import './style.css'
 
 
 
-// alert("Задание 6 нажмите ок чтобы продолжить")
+                          // alert("Задание 6 нажмите ок чтобы продолжить")
 // const exchangeRate = 0.93 // Курс обмена 1 доллар = 0,93 евро
 // let dollars = prompt("Введите сумму в долларах:")
 // let euros = dollars * exchangeRate
@@ -517,7 +547,7 @@ import './style.css'
 
 
 
-// alert("Задание 7 нажмите ок чтобы продолжить")
+                        // alert("Задание 7 нажмите ок чтобы продолжить")
 // let flashDriveSize = prompt("Укажите объем флешки в Гб:")
 // const fileSizeMB = 820
 // let flashDriveSizeMB = flashDriveSize * 1024
@@ -547,7 +577,7 @@ import './style.css'
 
 
 
-// alert("Задание 8 нажмите ок чтобы продолжить")
+                    // alert("Задание 8 нажмите ок чтобы продолжить")
 // let walletMoney = prompt("Введите сумму денег в вашем кошельке:")
 // let chocolatePrice = prompt("Введите цену одной шоколадки:")
 // let chocolatesCount = Math.floor(walletMoney / chocolatePrice)
@@ -579,7 +609,7 @@ import './style.css'
 
 
 
-// alert("Задание 10 нажмите ок чтобы продолжить")
+                           // alert("Задание 10 нажмите ок чтобы продолжить")
 // let number2 = prompt("Введите целое число:");
 // let isEven = number2 % 2 === 0;
 // let result = isEven ? "Число четное." : "Число нечетное.";
